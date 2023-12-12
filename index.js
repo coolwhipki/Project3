59d9515d0fe216209077ec3b63442401a7961509
coolwhipki <kirajanegirl@gmail.com>
2023-12-12 15:20:20 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let express = require("express");
+
+let app = require("express");
+
+app.get("/", (req,res) => res.send(""))
+
+app.listen(3000, () => console.log("Hey babbyy server is runnin sweet thang"))
