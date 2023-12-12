f32a72495029981ece3a0f16aa7b9937657c11d1
coolwhipki <kirajanegirl@gmail.com>
2023-12-12 15:26:14 -0700

    made stuff

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let express = require("express");
 
 let app = require("express");
 
-app.get("/", (req,res) => res.send(""))
-
+// random route to landing page
+app.get("/", (req, res) => {
+    res.render('index');
+});
 app.listen(3000, () => console.log("Hey babbyy server is runnin sweet thang"))
